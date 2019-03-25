c031c9683902167a1c13980729e243c11528a852
lucaspouchot <lucas.pouchot@gmail.com>
2019-03-25 16:39:34 +0100

    enable AP for 20 minutes instead of 10

--- index.js
+++ index.js
@@ -190,7 +190,7 @@ Interfaces.prototype.enableAcessPointOnWlan = function(ssid_id = 'ap'){
         this.setAccessPoint(ssid_id).then( () => {
           setTimeout( () => {
             this.setAccessPoint(false)
-          }, 600000)
+          }, 1200000)
           resolve(true)
         })
       } else if (ips.indexOf('192.168.61.1') !== -1){
@@ -202,7 +202,7 @@ Interfaces.prototype.enableAcessPointOnWlan = function(ssid_id = 'ap'){
         } else if(ips.length === 1){
           setTimeout( () => {
             this.setAccessPoint(false)
-          }, 600000)
+          }, 1200000)
           resolve(false)
         } else {
           resolve(false)
